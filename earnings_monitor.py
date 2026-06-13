1d4ef7ac46aad07cbe83ce87f6106ecf479980cf
74K4 <gaahi1225@gmail.com>
2026-06-13 21:03:48 +0900

    Update earnings_monitor.py

--- earnings_monitor.py
+++ earnings_monitor.py
@@ -1,14 +1,43 @@
 import os
 import requests
 
-webhook = os.environ["DISCORD_WEBHOOK"]
+API_KEY = os.environ["FMP_API_KEY"]
+WEBHOOK = os.environ["DISCORD_WEBHOOK"]
+
+url = f"https://financialmodelingprep.com/stable/earnings?symbol=NVDA&apikey={API_KEY}"
+
+data = requests.get(url).json()
+
+latest = None
+
+for row in data:
+    if row["epsActual"] is not None:
+        latest = row
+        break
+
+eps_beat = latest["epsActual"] > latest["epsEstimated"]
+rev_beat = latest["revenueActual"] > latest["revenueEstimated"]
 
 message = {
-    "content": "GitHub Actionsからテスト通知"
+    "content":
+    f"""@everyone
+
+NVDA
+
+Date: {latest['date']}
+
+EPS Actual: {latest['epsActual']}
+EPS Estimate: {latest['epsEstimated']}
+EPS Beat: {eps_beat}
+
+Revenue Actual: {latest['revenueActual']}
+Revenue Estimate: {latest['revenueEstimated']}
+Revenue Beat: {rev_beat}
+"""
 }
 
 response = requests.post(
-    webhook,
+    WEBHOOK,
     json=message
 )
 
