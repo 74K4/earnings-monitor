f112f423e2a1cebe65d0b100bc9943a9f7e40a5c
74K4 <gaahi1225@gmail.com>
2026-06-13 13:59:27 +0900

    Update earnings_monitor.py

--- earnings_monitor.py
+++ earnings_monitor.py
@@ -1,20 +1,15 @@
 import os
 import requests
 
-API_KEY = os.environ["FMP_API_KEY"]
+webhook = os.environ["DISCORD_WEBHOOK"]
 
-url = f"https://financialmodelingprep.com/stable/earnings?symbol=NVDA&apikey={API_KEY}"
+message = {
+    "content": "GitHub Actionsからテスト通知"
+}
 
-data = requests.get(url).json()
+response = requests.post(
+    webhook,
+    json=message
+)
 
-latest = None
-
-for row in data:
-    if row["epsActual"] is not None:
-        latest = row
-        break
-
-print("Ticker:", latest["symbol"])
-print("Date:", latest["date"])
-print("EPS Actual:", latest["epsActual"])
-print("EPS Estimate:", latest["epsEstimated"])
+print(response.status_code)
