e6d0ff4f28ab5dffff2cfba47e4c69530ff31f72
74K4 <gaahi1225@gmail.com>
2026-06-13 21:16:09 +0900

    Update earnings_monitor.py

--- earnings_monitor.py
+++ earnings_monitor.py
@@ -2,43 +2,45 @@ import os
 import requests
 
 API_KEY = os.environ["FMP_API_KEY"]
-WEBHOOK = os.environ["DISCORD_WEBHOOK"]
 
-url = f"https://financialmodelingprep.com/stable/earnings?symbol=NVDA&apikey={API_KEY}"
+stocks = [
+    "NVDA",
+    "META",
+    "AMZN",
+    "GOOGL",
+    "MSFT",
+    "LAR"
+]
 
-data = requests.get(url).json()
+for stock in stocks:
 
-latest = None
+    print("==========")
+    print(stock)
 
-for row in data:
-    if row["epsActual"] is not None:
-        latest = row
-        break
+    url = f"https://financialmodelingprep.com/stable/earnings?symbol={stock}&apikey={API_KEY}"
 
-eps_beat = latest["epsActual"] > latest["epsEstimated"]
-rev_beat = latest["revenueActual"] > latest["revenueEstimated"]
+    data = requests.get(url).json()
 
-message = {
-    "content":
-    f"""@everyone
+    latest = None
 
-NVDA
+    for row in data:
 
-Date: {latest['date']}
+        if row["epsActual"] is not None:
 
-EPS Actual: {latest['epsActual']}
-EPS Estimate: {latest['epsEstimated']}
-EPS Beat: {eps_beat}
+            latest = row
 
-Revenue Actual: {latest['revenueActual']}
-Revenue Estimate: {latest['revenueEstimated']}
-Revenue Beat: {rev_beat}
-"""
-}
+            break
 
-response = requests.post(
-    WEBHOOK,
-    json=message
-)
+    if latest is None:
 
-print(response.status_code)
+        print("決算データなし")
+
+        continue
+
+    eps_beat = latest["epsActual"] > latest["epsEstimated"]
+
+    rev_beat = latest["revenueActual"] > latest["revenueEstimated"]
+
+    print("Date:", latest["date"])
+    print("EPS Beat:", eps_beat)
+    print("Revenue Beat:", rev_beat)
